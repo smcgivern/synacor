da10b879c0f7864dbe860ecf0f538e41b5043e2b
Sean McGivern <sean@mcgivern.me.uk>
2020-11-29 16:36:57 +0000

    Add explain command

--- synacor.zig
+++ synacor.zig
@@ -1,7 +1,9 @@
 const std = @import("std");
 
 // First command-line argument: memory dump (defaults to `challenge.bin`)
-// Second command-line argument: if `history`, writes a history file
+// Second command-line argument: command
+//   - If `history`, writes a history file
+//   - If `explain`, writes commands to stderr
 //
 // Memory dump is written when stdin ends (C-d or redirect stdin from history
 // file)
@@ -36,6 +38,32 @@ fn arg(memory: anytype, registers: anytype, index: usize) u16 {
         return value;
     } else {
         return registers[value - 32768];
+    }
+}
+
+fn explainArg(memory: anytype, registers: anytype, index: usize) []u8 {
+    const value = memory[index];
+
+    if (value < 32768) {
+        return std.fmt.allocPrint(std.heap.page_allocator, "{}", .{value}) catch unreachable;
+    } else {
+        return std.fmt.allocPrint(std.heap.page_allocator, "R{}", .{value - 32768}) catch unreachable;
+    }
+}
+
+fn printExplain(cmd: anytype, explain: bool, stream: anytype, memory: anytype, registers: anytype, i: usize, args: usize) !void {
+    if (!explain) {
+        return;
+    }
+
+    if (args == 0) {
+        try stream.print("{}\n", .{cmd});
+    } else if (args == 1) {
+        try stream.print("{} {}\n", .{ cmd, explainArg(memory, registers, i + 1) });
+    } else if (args == 2) {
+        try stream.print("{} {} {}\n", .{ cmd, explainArg(memory, registers, i + 1), explainArg(memory, registers, i + 2) });
+    } else if (args == 3) {
+        try stream.print("{} {} {} {}\n", .{ cmd, explainArg(memory, registers, i + 1), explainArg(memory, registers, i + 2), explainArg(memory, registers, i + 3) });
     }
 }
 
@@ -146,23 +174,23 @@ pub fn main() !void {
     var memory: [32768]u16 = undefined;
     var registers = [_]u16{0} ** 8;
     var stack = Stack{};
+    var i = try load(getArg(0), &memory, &registers, &stack);
 
     const stdout = std.io.getStdOut().outStream();
+    const stderr = std.io.getStdErr().outStream();
     const stdin = std.io.getStdIn().inStream();
+    const cmd = getArg(1);
+    const explain = std.mem.eql(u8, cmd, "explain");
 
-    var history = historyFile(getArg(1));
-
+    const history = historyFile(cmd);
     defer if (history) |h| {
         h.close();
     };
 
-    var i = try load(getArg(0), &memory, &registers, &stack);
     var opcode: u16 = 0;
     var a_: u16 = 0;
     var a: u16 = 0;
-    var b_: u16 = 0;
     var b: u16 = 0;
-    var c_: u16 = 0;
     var c: u16 = 0;
 
     while (true) {
@@ -177,36 +205,56 @@ pub fn main() !void {
 
         switch (opcode) {
             // halt
-            0 => break,
+            0 => {
+                try printExplain("halt", explain, stderr, memory, registers, i, 0);
+
+                break;
+            },
             // set
             1 => {
+                try printExplain("set", explain, stderr, memory, registers, i, 2);
+
                 registers[a_] = b;
                 i += 3;
             },
             // push
             2 => {
+                try printExplain("push", explain, stderr, memory, registers, i, 1);
+
                 stack.push(a);
                 i += 2;
             },
             // pop
             3 => {
+                try printExplain("pop", explain, stderr, memory, registers, i, 1);
+
                 registers[a_] = stack.pop();
                 i += 2;
             },
             // eq
             4 => {
+                try printExplain("eq", explain, stderr, memory, registers, i, 3);
+
                 registers[a_] = @boolToInt(b == c);
                 i += 4;
             },
             // gt
             5 => {
+                try printExplain("gt", explain, stderr, memory, registers, i, 3);
+
                 registers[a_] = @boolToInt(b > c);
                 i += 4;
             },
             // jmp
-            6 => i = a,
+            6 => {
+                try printExplain("jmp", explain, stderr, memory, registers, i, 1);
+
+                i = a;
+            },
             // jt
             7 => {
+                try printExplain("jt", explain, stderr, memory, registers, i, 2);
+
                 if (a != 0) {
                     i = b;
                 } else {
@@ -215,6 +263,8 @@ pub fn main() !void {
             },
             // jf
             8 => {
+                try printExplain("jf", explain, stderr, memory, registers, i, 2);
+
                 if (a == 0) {
                     i = b;
                 } else {
@@ -223,63 +273,86 @@ pub fn main() !void {
             },
             // add
             9 => {
+                try printExplain("add", explain, stderr, memory, registers, i, 3);
+
                 registers[a_] = mod(b + c);
                 i += 4;
             },
             // mult
             10 => {
+                try printExplain("mult", explain, stderr, memory, registers, i, 3);
+
                 registers[a_] = @intCast(u16, (@intCast(u32, b) * @intCast(u32, c)) % 32768);
                 i += 4;
             },
             // mod
             11 => {
+                try printExplain("mod", explain, stderr, memory, registers, i, 3);
+
                 registers[a_] = mod(b % c);
                 i += 4;
             },
             // and
             12 => {
+                try printExplain("and", explain, stderr, memory, registers, i, 3);
+
                 registers[a_] = b & c;
                 i += 4;
             },
             // or
             13 => {
+                try printExplain("or", explain, stderr, memory, registers, i, 3);
+
                 registers[a_] = b | c;
                 i += 4;
             },
             // not
             14 => {
+                try printExplain("not", explain, stderr, memory, registers, i, 2);
+
                 registers[a_] = mod(~b);
                 i += 3;
             },
             // rmem
             15 => {
+                try printExplain("rmem", explain, stderr, memory, registers, i, 2);
+
                 registers[a_] = memory[b];
                 i += 3;
             },
             // wmem
             16 => {
+                try printExplain("wmem", explain, stderr, memory, registers, i, 2);
+
                 memory[a] = b;
                 i += 3;
             },
             // call
             17 => {
+                try printExplain("call", explain, stderr, memory, registers, i, 1);
+
                 stack.push(@intCast(u16, i + 2));
                 i = a;
             },
             // ret
             18 => {
+                try printExplain("ret", explain, stderr, memory, registers, i, 0);
+
                 i = stack.pop();
             },
             // out
             19 => {
+                try printExplain("out", explain, stderr, memory, registers, i, 1);
+
                 _ = try std.fmt.formatAsciiChar(@intCast(u8, a), .{}, stdout);
                 i += 2;
             },
             // in
             20 => {
+                try printExplain("in", explain, stderr, memory, registers, i, 1);
+
                 if (stdin.readByte()) |input| {
                     registers[a_] = input;
-                    i += 2;
 
                     if (history) |h| {
                         _ = try h.write(&[1]u8{input});
@@ -288,9 +361,15 @@ pub fn main() !void {
                     try dump(memory, registers, stack, i);
                     return err;
                 }
+
+                i += 2;
             },
             // noop
-            21 => i += 1,
+            21 => {
+                try printExplain("noop", explain, stderr, memory, registers, i, 0);
+
+                i += 1;
+            },
             else => {
                 try stdout.print("unhandled opcode: {}\n", .{opcode});
                 break;
